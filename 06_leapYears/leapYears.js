4787ec501c49a8c612ae039c15decc59d0a0c6ce
oioiyoko <suziekang96@gmail.com>
2023-12-26 18:14:57 +0900

    Complete exercises

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,4 +1,16 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    if (year % 4 == 0) {
+        if (year % 400 == 0) {
+            return true
+        }
+        else if (year % 100 ==0) {
+            return false
+        }
+        else {
+            return true
+        }
+    }
+    return false;
 
 };
 
--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,27 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fah) {
+  let cel = 0;
+  cel = (fah - 32)*(5/9);
+  console.log("THIS IS CEL: " + cel);
+  if (Number.isInteger(cel)) {
+    return Number(cel);
+  }
+  else {
+    cel = cel.toFixed(1);
+  }
+  return Number(cel);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(cel) {
+  let fah = 0;
+  fah = (cel * 9/5) + 32;
+  if (Number.isInteger(cel)) {
+    return Number(fah);
+  }
+  else {
+    fah = fah.toFixed(1);
+  }
+  return Number(fah);
+
 };
 
 // Do not edit below this line
